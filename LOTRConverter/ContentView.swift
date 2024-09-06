1e61c097c4efa7bf240fad35b5aab245a5bbf23f
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-09-06 09:42:37 +0600

    Added ZStack, VStack, HStack in design.

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -9,14 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-        } 
-        .padding()
+        ZStack{
+            //
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                HStack{
+                    VStack {
+                        HStack {
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 35)
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                        }
+                        Text("Textfield").foregroundStyle(.white)
+                    }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    VStack {
+                        HStack {
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 35)
+                          
+                            
+                        }
+                        Text("Textfield").foregroundStyle(.white)
+                    }
+                }
+                Spacer()
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .
+            }
+        }
     }
 }
 
