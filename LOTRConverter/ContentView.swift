a0bbc6e30eddc182e3da672d227dc443aeeca21f
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-09-06 08:48:11 +0600

    Initial Commit

---
+++ LOTRConverter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Ibrahim Lokman on 6/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+        } 
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Ibrahim Lokman on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
